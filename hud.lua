ff2233feb8f0cc9a559c60ef3df8925c679c951e
Axa <a9xa@hotmail.com>
2019-08-06 00:45:04 +0300

    Update hud.lua

--- hud.lua
+++ hud.lua
@@ -4,6 +4,7 @@
 LosFaul for his contribution.
 osnapitzjamie for his contribution.
 Dutchman101's for PNG optimizer tool (icons) / https://forum.mtasa.com/topic/119032-tool-png-optimizer/
+AfuSensi for his contribution.
 
 Commands List:
 displayhud - toggles between showing and hiding the HUD.
